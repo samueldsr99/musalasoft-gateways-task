6f07ffd760f6af8d07484999d2077b5afe5c2f15
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-01-16 01:17:37 -0500

    refactor: Remove external db config

--- server/config/prisma.ts
+++ server/config/prisma.ts
@@ -1,9 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: { url: `${process.env.DATABASE_URL}/${process.env.NODE_ENV}` },
-  },
-});
+const prisma = new PrismaClient();
 
 export default prisma;
